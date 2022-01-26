6411bf15cd6db953409077a8b7faf23b74b2c929
neverswood <nevers_wood@hotmail.fr>
2022-01-26 14:51:36 +0100

    Display recipes

--- index.js
+++
@@ -1,7 +0,0 @@
-import { recipes } from "./data/recipes.js";
-
-function index() {
-  for (let index = 0; index < recipes.length; index++) {}
-}
-
-index();
---
+++ js/index.js
@@ -0,0 +1,9 @@
+import { recipes } from "./data/recipes.js";
+
+function index() {
+  for (let index = 0; index < recipes.length; index++) {
+    displayRecipes(recipes[index]);
+  }
+}
+
+index();
